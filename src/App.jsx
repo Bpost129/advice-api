734b05b56e4fba6f6a82e3d5b7aeb25ca849a858
Bpost129 <Bpost129@yahoo.com>
2025-05-05 22:58:00 -0400

    call service function for trivia data in useEffect, log return data

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,29 @@ import Advice from './pages/Advice/Advice'
 import Trivia from './pages/Trivia/Trivia'
 
 import { getAdvice } from "./services/advice-api"
+import { getTriviaQuestions } from './services/trivia-api'
 
 import './App.css'
 
 function App() {
   const [quote, setQuote] = useState({})
+  const [questions, setQuestions] = useState({})
   
   const fetchAdvice = async () => {
     const adviceData = await getAdvice()
     setQuote(adviceData.slip.advice)
     console.log('quote::' + adviceData.slip.advice)
   }
+
+  const fetchTrivia = async () => {
+    const questionData = await getTriviaQuestions()
+    setQuestions(questionData)
+    console.log(questionData)
+  }
   
   useEffect(() => {
     fetchAdvice()
+    fetchTrivia()
   }, [])
 
   if (!quote.length) return <h1>Want Some Advice?</h1>
