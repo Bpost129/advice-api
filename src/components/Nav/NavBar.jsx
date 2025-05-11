3efd19ffb7227701deaa057fa2707c7c0872c6e3
Bpost129 <Bpost129@yahoo.com>
2025-05-10 23:24:06 -0400

    add joke route to navbar, render joke page

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -5,6 +5,7 @@ const NavBar = () => {
     <div id="navbar">
       <NavLink to='/'>Home</NavLink>
       <NavLink to='/advice'>Advice</NavLink>
+      <NavLink to='/jokes'>Jokes</NavLink>
       <NavLink to='/trivia'>Trivia</NavLink>
     </div>
   )
