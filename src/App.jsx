1fe199631ac4a760f0ba5d2202f75db7c06500b7
Bpost129 <Bpost129@yahoo.com>
2025-05-10 23:22:33 -0400

    import Joke page to app and create route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from './components/Nav/NavBar'
 import Landing from './pages/Landing/Landing'
 import Advice from './pages/Advice/Advice'
 import Trivia from './pages/Trivia/Trivia'
+import Jokes from './pages/Jokes/Jokes'
 
 import './App.css'
 
@@ -17,6 +18,7 @@ function App() {
           <Route path='/' element={<Landing />} />
           <Route path='/advice' element={<Advice />} />
           <Route path='/trivia' element={<Trivia />} />
+          <Route path='/jokes' element={<Jokes />} />
         </Routes>
       </main>
     </>
